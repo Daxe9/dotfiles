d91fcfc9de5b287214453c203a96ac4f653144d3
_xie <davidexie694@gmail.com>
2022-12-17 15:47:39 +0100

    deleted nvim

--- nvim/lua/_xie/init.lua
+++
@@ -1,4 +0,0 @@
-require('_xie.lspconfig')
-require('_xie.treesitter')
-require('_xie.telescope')
-require('_xie.cmp')
--- nvim/lua/_xie/treesitter.lua
+++
@@ -1,8 +0,0 @@
-local treesitter = require('nvim-treesitter.configs')
-
-treesitter.setup {
-	highlight = {
-		enalble = true
-	},
-	auto_install = true
-}
